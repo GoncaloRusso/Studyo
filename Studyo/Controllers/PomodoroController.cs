515e11334a41ad06d3bac7a4fbdb1978ec6061d1
P-Rafael <201801854@estudantes.ips.pt>
2024-02-08 20:40:23 +0000

    Pomodoro
    Implementação da interface do pomodoro

---
+++ Studyo/Controllers/PomodoroController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Studyo.Models;
+
+namespace Studyo.Controllers
+{
+    public class PomodoroController : Controller
+    {
+        private Pomodoro pomodoro;
+
+        public PomodoroController()
+        {
+            pomodoro = new Pomodoro();
+            pomodoro.StudyTime = 0;
+            pomodoro.RestTime = 0;
+            pomodoro.Cycles = 0;
+        }
+
+        public IActionResult Index()
+        {
+            return View(pomodoro);
+        }
+
+
+    }
+}
