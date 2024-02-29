4bce8399c68872de2f0c438e4ac121213c80757d
Duarte Alcácer <dudalcacer@outlook.com>
2024-02-29 15:17:44 +0000

    Mostrar conteudo na pdisciplina

--- Studyo/Controllers/MateriaController.cs
+++ Studyo/Controllers/MateriaController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Studyo.Data;
 using Studyo.Models;
+using System.Drawing.Drawing2D;
+using System.Text.RegularExpressions;
 
 namespace Studyo.Controllers
 {
@@ -13,6 +15,7 @@ namespace Studyo.Controllers
             _context = context;
         }
 
+        
         public IActionResult MateriaSelecionada(int id)
         {
             // Acesse a base de dados para obter a matéria pelo ID
@@ -23,6 +26,20 @@ namespace Studyo.Controllers
 
             return View(materia);
         }
+        
+        public IActionResult ConteudoEscolhido (int id)
+        {
+            var chap = _context.Chapters.Where(c => c.Id == id).FirstOrDefault();
 
+            if (chap == null) { return NotFound(); }
+
+            chap.Normal = _context.Materials.Where(m => m.ChapterId == chap.Id).FirstOrDefault();
+
+            Regex.Replace(chap.Normal.Content,@"\\r\\n|\\r|\\n", Environment.NewLine);
+
+            ViewBag.SelectedId = id;
+
+            return View(chap);
+        }
     }
 }
