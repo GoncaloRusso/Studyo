7a2dc4abaaaca8d4febae5c05e62313f95d347eb
P-Rafael <201801854@estudantes.ips.pt>
2024-02-29 20:54:13 +0000

    Create DiscUser if not exists
    Cria um novo DisciplinaUser para aquele user e Disciplina caso seja a primeira vez que este está a abrir a disciplina

--- Studyo/Controllers/MateriaController.cs
+++ Studyo/Controllers/MateriaController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Studyo.Data;
 using Studyo.Models;
 using System.Drawing.Drawing2D;
@@ -9,20 +10,47 @@ namespace Studyo.Controllers
     public class MateriaController : Controller
     {
         private readonly StudyoDbContext _context;
+        private readonly UserManager<IdentityUser> _userManger;
 
-        public MateriaController(StudyoDbContext context)
+        public MateriaController(StudyoDbContext context, UserManager<IdentityUser> userManager)
         {
             _context = context;
+            _userManger = userManager;
         }
 
         
-        public IActionResult MateriaSelecionada(int id)
+        public async Task<IActionResult> MateriaSelecionada(int id)
         {
+            IdentityUser user = await _userManger.GetUserAsync(User);
+
             // Acesse a base de dados para obter a matéria pelo ID
             var materia = _context.Subjects.Where(c => c.Id == id).FirstOrDefault();
             materia.Chapters = _context.Chapters.Where(c => c.SubjectId == id).ToList();
 
             if (materia == null) { return NotFound(); }
+
+            var discUser = _context.DisciplinaUsers.Where(d => d.UserId == user.Id && d.DisciplinaId == id).FirstOrDefault();
+
+            if (discUser == null)
+            {
+                DisciplinaUser du = new DisciplinaUser();
+                
+                du.DisciplinaId = id;
+                
+                du.UserId = user.Id;
+                
+                Dictionary<Chapter, bool> mc = new Dictionary<Chapter, bool>();
+                foreach (Chapter c in materia.Chapters)
+                {
+                    mc.Add(c, false);
+                }
+                du.MaterialComp = mc;
+
+                _context.DisciplinaUsers.Add(du);
+
+            }
+
+
 
             return View(materia);
         }
