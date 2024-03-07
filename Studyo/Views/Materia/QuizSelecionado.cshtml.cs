99c3687f723983a2e1293d4a53878ddc23164a08
GoncaloRusso <72861354+GoncaloRusso@users.noreply.github.com>
2024-03-07 21:49:13 +0000

    alteração do duarte

---
+++ Studyo/Views/Materia/QuizSelecionado.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Studyo.Views.Materia
+{
+    public class QuizSelecionadoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
